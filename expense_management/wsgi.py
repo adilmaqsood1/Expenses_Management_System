b9e93e5f8ca3e75277508293cb18f3f70fa92261
adilmaqsood1 <126709358+adilmaqsood1@users.noreply.github.com>
2025-03-19 00:27:40 +0500

    app added in wsgi.py

--- expense_management/wsgi.py
+++ expense_management/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expense_management.settings')
 
 application = get_wsgi_application()
+
+# This is needed for Vercel deployment
+app = application
