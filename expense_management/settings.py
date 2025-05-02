30dc9ba3a3a85ce701dd38f165cb0e5c1df26bca
adilmaqsood1 <126709358+adilmaqsood1@users.noreply.github.com>
2025-05-03 00:35:04 +0500

    removed manifest in settings

--- expense_management/settings.py
+++ expense_management/settings.py
@@ -169,7 +169,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if os.getenv("DATABASE_URL"):
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    # Use a more lenient storage class that doesn't validate file references
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+    
+    # Add static files finders to help locate files
+    STATICFILES_FINDERS = [
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
