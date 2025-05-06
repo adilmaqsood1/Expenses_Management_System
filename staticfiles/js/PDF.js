936b932a01a75a688f17269d2bde67364f7c5bb5
adilmaqsood1 <126709358+adilmaqsood1@users.noreply.github.com>
2025-05-07 00:05:14 +0500

    mis dashboard modified

---
+++ staticfiles/js/PDF.js
@@ -0,0 +1,29 @@
+﻿window.onload = function () {
+    document.getElementById("download")
+        .addEventListener("click", () => {
+            const invoice = document.getElementById("invoice");
+            var opt = {
+                margin: 5,
+                filename: 'expenditure_claim.pdf',
+                image: { type: 'jpeg', quality: 0.95 },
+                html2canvas: { scale: 2 },
+                jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+            };
+            html2pdf().from(invoice).set(opt).save();
+        });
+
+ document.getElementById("print")
+        .addEventListener("click", () => {
+            const invoice = document.getElementById("invoice");
+            const printWindow = window.open('', '', 'height=600,width=800');
+            printWindow.document.write('<html><head><title>Invoice</title>');
+            printWindow.document.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">'); // Bootstrap CSS
+            printWindow.document.write('<style>body{margin: 20px;}</style>'); // Optional custom styles
+            printWindow.document.write('</head><body>');
+            printWindow.document.write(invoice.innerHTML);
+            printWindow.document.write('</body></html>');
+            printWindow.document.close();
+            printWindow.print();
+        });
+};
+
